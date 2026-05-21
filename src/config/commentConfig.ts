86e7484414481444c372afa4cfbe107b92273513
z19163208515-zhouxiang <z19163208515@gmail.com>
2026-05-21 23:03:29 +0800

    这是我的博客

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -23,7 +23,7 @@ export const commentConfig: CommentConfig = {
 	//waline评论系统配置
 	waline: {
 		// waline 后端服务地址
-		serverURL: "https://waline.vercel.app",
+		serverURL: "https://comment.zhouxpro.xyz/",
 		// 设置 Waline 评论系统语言
 		lang: "zh-CN",
 		// 设置 Waline 评论系统表情地址
